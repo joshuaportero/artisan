85abcb8f14bc73ad78608d66efb089ae66911eaf
Joshua Portero <77929472+joshuaportero@users.noreply.github.com>
2024-11-18 12:02:59 -0500

    add: cors filter

--- src/main/java/dev/portero/artisan/security/config/SecurityConfiguration.java
+++ src/main/java/dev/portero/artisan/security/config/SecurityConfiguration.java
@@ -1,26 +1,54 @@
 package dev.portero.artisan.security.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfiguration {
 
+    @Value("#{'${application.security.cors.allowed-origins:*}'.split(',')}")
+    private List<String> allowedOrigins;
+
+    @Value("#{'${application.security.cors.allowed-methods:*}'.split(',')}")
+    private List<String> allowedMethods;
+
+    @Value("#{'${application.security.cors.allowed-headers:*}'.split(',')}")
+    private List<String> allowedHeaders;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/**").permitAll()
                         .anyRequest()
                         .authenticated()
                 );
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(allowedOrigins);
+        configuration.setAllowedMethods(allowedMethods);
+        configuration.setAllowedHeaders(allowedHeaders);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/v1/**", configuration);
+        return source;
     }
 }
